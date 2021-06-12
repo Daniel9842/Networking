f835b2e28db27265a80372c63194530cc6988653
Daniel Ducuara <daniel.ducuara@mail.escuelaing.edu.co>
2021-06-11 20:28:15 -0500

    Source code

--- src/main/java/edu/escuelaing/arsw/Networking/HttpServer/HttpServer.java
+++ src/main/java/edu/escuelaing/arsw/Networking/HttpServer/HttpServer.java
@@ -114,7 +114,7 @@ public class HttpServer {
 		}else if(path.endsWith(".js")) {
 			type = "text/javascript";
 		}
-		Path file = Paths.get("."+path);
+		Path file = Paths.get("./TestHttpServer"+path);
 		Charset charset = Charset.forName("UTF-8");
 		String outmsg="";
 		try(BufferedReader reader = Files.newBufferedReader(file,charset)){
